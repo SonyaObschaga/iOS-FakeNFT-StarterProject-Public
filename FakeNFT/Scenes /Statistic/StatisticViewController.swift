8acd8b9f5ef6ad1deb0a4752152626c4392e182e
baxeezy <danya.bahv@yandex.ru>
2025-11-24 11:06:26 +0300

    m1-30 добавил в TabBar статистику

---
+++ FakeNFT/Scenes /Statistic/StatisticViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+final class StatisticViewController: UIViewController {
+    
+    let servicesAssembly: ServicesAssembly
+    let testNftButton = UIButton()
+    
+    init(servicesAssembly: ServicesAssembly) {
+        self.servicesAssembly = servicesAssembly
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -9,6 +9,12 @@ final class TabBarController: UITabBarController {
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
+    
+    private let statisticTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.statistic", comment: ""),
+        image: UIImage(systemName: "flag.2.crossed.fill"),
+        tag: 3
+        )
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,9 +22,15 @@ final class TabBarController: UITabBarController {
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
+        
+        let statisticController = StatisticViewController(
+            servicesAssembly: servicesAssembly
+        )
+        
+        statisticController.tabBarItem = statisticTabBarItem
         catalogController.tabBarItem = catalogTabBarItem
 
-        viewControllers = [catalogController]
+        viewControllers = [catalogController, statisticController]
 
         view.backgroundColor = .systemBackground
     }
