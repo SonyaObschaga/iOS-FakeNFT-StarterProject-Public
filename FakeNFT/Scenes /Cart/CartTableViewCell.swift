117a1155dde75c31b0b38d50da7e1a86200d690e
socloudy-dev <vlad98.com@gmail.com>
2025-11-27 00:28:59 +0300

    feat(cartCell): Сверстана ячейка для TableView

--- FakeNFT/Scenes /Cart/CartTableViewCell.swift
+++ FakeNFT/Scenes /Cart/CartTableViewCell.swift
@@ -6,7 +6,9 @@ class CartTableViewCell: UITableViewCell {
     
     private let cellView: UIView = {
         let container = UIView()
-        container.backgroundColor = UIColor(resource: .white)
+        container.backgroundColor = .backgroundPrimary
+        container.layer.cornerRadius = 12
+        container.clipsToBounds = true
         container.translatesAutoresizingMaskIntoConstraints = false
         return container
     }()
@@ -21,13 +23,14 @@ class CartTableViewCell: UITableViewCell {
     private let nftNameLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 17, weight: .bold)
-        label.textColor = UIColor(resource: .black)
+        label.textColor = .textPrimary
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let ratingOfNftImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(resource: .rating0)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -35,7 +38,7 @@ class CartTableViewCell: UITableViewCell {
     private let costTitleLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 13, weight: .regular)
-        label.textColor = UIColor(resource: .black)
+        label.textColor = .textPrimary
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -43,15 +46,57 @@ class CartTableViewCell: UITableViewCell {
     private let costOfNftLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 17, weight: .bold)
-        label.textColor = UIColor(resource: .black)
+        label.textColor = .textPrimary
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let deleteNftButton: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(resource: .deletFromCart), for: .normal)
+        button.setImage(UIImage(resource: .deleteFromCart), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    private func setupViews() {
+        contentView.addSubview(cellView)
+        cellView.addSubview(nftImageView)
+        cellView.addSubview(nftNameLabel)
+        cellView.addSubview(ratingOfNftImageView)
+        cellView.addSubview(costTitleLabel)
+        cellView.addSubview(costOfNftLabel)
+        cellView.addSubview(deleteNftButton)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            cellView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            cellView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            cellView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            cellView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+            
+            nftImageView.heightAnchor.constraint(equalToConstant: 108),
+            nftImageView.widthAnchor.constraint(equalToConstant: 108),
+            nftImageView.leadingAnchor.constraint(equalTo: cellView.leadingAnchor),
+            
+            nftNameLabel.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 8),
+            nftNameLabel.leadingAnchor.constraint(equalTo: nftImageView.trailingAnchor, constant: 20),
+            
+            ratingOfNftImageView.topAnchor.constraint(equalTo: cellView.topAnchor, constant: 34),
+            ratingOfNftImageView.leadingAnchor.constraint(equalTo: nftImageView.trailingAnchor, constant: 20),
+            
+            costTitleLabel.topAnchor.constraint(equalTo: ratingOfNftImageView.bottomAnchor, constant: 12),
+            costTitleLabel.leadingAnchor.constraint(equalTo: nftImageView.trailingAnchor, constant: 20),
+            
+            costOfNftLabel.topAnchor.constraint(equalTo: costTitleLabel.bottomAnchor, constant: 2),
+            costOfNftLabel.leadingAnchor.constraint(equalTo: nftImageView.trailingAnchor, constant: 20),
+            
+            deleteNftButton.centerYAnchor.constraint(equalTo: cellView.centerYAnchor),
+            deleteNftButton.trailingAnchor.constraint(equalTo: cellView.trailingAnchor),
+            deleteNftButton.heightAnchor.constraint(equalToConstant: 40),
+            deleteNftButton.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    
 }
