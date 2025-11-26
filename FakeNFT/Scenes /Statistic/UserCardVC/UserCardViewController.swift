4e86862b38d3142d273509b0f92eca6b293f26a9
baxeezy <danya.bahv@yandex.ru>
2025-11-26 21:03:45 +0300

    m1-36 верстка карточки пользователя 1/2

--- FakeNFT/Scenes /Statistic/UserCardVC/UserCardViewController.swift
+++ FakeNFT/Scenes /Statistic/UserCardVC/UserCardViewController.swift
@@ -16,8 +16,21 @@ final class UserCardViewController: UIViewController {
         return button
     }()
     
+    private lazy var avatarImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "person.crop.circle.fill")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        
+        return imageView
+    }()
+    
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
+        label.text = "Joaquin Phoenix"
         label.font = .headline3
         label.textColor = .ypBlackLight
         return label
@@ -25,9 +38,27 @@ final class UserCardViewController: UIViewController {
     
     private lazy var descriptionLabel: UILabel = {
         let label = UILabel()
+        label.text = "Дизайнер из Казани, люблю цифровое искусство и бейглы. В моей коллекции уже 100+ NFT,и еще больше — на моём сайте. Открыт к коллаборациям."
         label.font = .caption2
         label.textColor = .ypBlackLight
+        label.numberOfLines = 4
         return label
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [avatarImageView, nameLabel])
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
+    private lazy var verticalStackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [stackView, descriptionLabel])
+        stack.axis = .vertical
+        stack.spacing = 20
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
     }()
     
     // MARK: - Lifecycle
@@ -46,6 +77,7 @@ final class UserCardViewController: UIViewController {
     private func setupUI() {
         view.backgroundColor = .systemBackground
         setupBackwardButton()
+        setupVerticalStackView()
     }
     
     private func setupBackwardButton() {
@@ -58,11 +90,28 @@ final class UserCardViewController: UIViewController {
             backwardButton.heightAnchor.constraint(equalToConstant: 24)
         ])
     }
+    
+    private func setupVerticalStackView() {
+        view.addSubview(verticalStackView)
+        
+        NSLayoutConstraint.activate([
+            verticalStackView.topAnchor.constraint(equalTo: backwardButton.bottomAnchor, constant: 29),
+            verticalStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            verticalStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            
+            stackView.leadingAnchor.constraint(equalTo: verticalStackView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: verticalStackView.trailingAnchor),
+            
+            avatarImageView.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
+        ])
+    }
 }
 
 extension UserCardViewController: UserCardViewProtocol {
     func displayUser(_ user: User) {
-        nameLabel.text = user.name
-        descriptionLabel.text = user.name // поменять
+//        nameLabel.text = user.name
+        nameLabel.text = "Test Name" // ← Временный текст для теста
+        descriptionLabel.text = "Test Description" // поменять
     }
 }
