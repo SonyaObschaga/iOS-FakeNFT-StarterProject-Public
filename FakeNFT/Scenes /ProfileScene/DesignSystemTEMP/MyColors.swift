94a00db8e48c31160274d14a64098606883f167b
Damir Salakhetdinov <saldam@inbox.ru>
2025-11-28 19:10:16 +0300

    feat: добавлена ProfileScene вместе с необходимыми зависимостями

---
+++ FakeNFT/Scenes /ProfileScene/DesignSystemTEMP/MyColors.swift
@@ -0,0 +1,19 @@
+//
+//  MyColors.swift
+//  FakeNFT
+//
+//  Created by Damir Salakhetdinov on 23/11/25.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var ypBlackDay: UIColor! { UIColor(named: "Black [day]")}
+    static var ypLightGreyDay: UIColor! { UIColor(named: "Light grey [day]")}
+    static var ypWhiteDay: UIColor! { UIColor(named: "White [day]")}
+    
+    static var ypBlueUniversal: UIColor! { UIColor(named: "Blue Universal")}
+    static var ypWhiteUniversal: UIColor! { UIColor(named: "White Universal")}
+    
+}
