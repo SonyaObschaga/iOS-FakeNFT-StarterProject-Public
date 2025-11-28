a959ffe9306689c9263da660664d9c4050c1e9cb
Damir Salakhetdinov <saldam@inbox.ru>
2025-11-28 19:20:30 +0300

    Добавлена модель профиля

---
+++ FakeNFT/Models/ProfileModel/NFT.swift
@@ -0,0 +1,24 @@
+//
+//  NFT.swift
+//  TEST_TOOL2
+//
+//  Created by Damir Salakhetdinov on 24/11/25.
+//
+
+import Foundation
+
+//class NFT: Codable {
+class NFT: Encodable, Decodable {
+    init() {
+        createdAt = Date()
+    }
+    var createdAt: Date
+    var name: String = ""
+    var images: [String] = []
+    var rating: Int? = 0
+    var description: String? = ""
+    var price: Float = 0
+    var author: String = ""
+    var id: String = ""
+    var isLiked: Bool = false
+}
---
+++ FakeNFT/Models/ProfileModel/ProfileCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileCellModel.swift
+//  FakeNFT
+//
+//  Created by Damir Salakhetdinov on 23/11/25.
+//
+
+import Foundation
+
+struct ProfileCellModel {
+    let name: String
+    let count: Int?
+    let action: () -> Void
+}
