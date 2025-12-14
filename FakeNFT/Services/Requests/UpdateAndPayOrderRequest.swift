b6b7ee796dd36955d2ffcdb28b68de9f69cecaff
socloudy-dev <vlad98.com@gmail.com>
2025-12-14 14:34:28 +0300

    feat(updateAndPayRequest): Добавлен новый Request, который будет использован для редактирования списка корзины и оплаты

---
+++ FakeNFT/Services/Requests/UpdateAndPayOrderRequest.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+struct UpdateOrderDto: Dto {
+    let nftIds: [String]
+
+    func asDictionary() -> [String: String] {
+        [
+            "nfts": nftIds.joined(separator: ",")
+        ]
+    }
+}
+
+struct UpdateOrderRequest: NetworkRequest {
+
+    struct DtoModel: Dto {
+        let nftIds: [String]
+
+        func asDictionary() -> [String: String] {
+            [
+                "nfts": nftIds.joined(separator: ",")
+            ]
+        }
+    }
+
+    let nftIds: [String]
+
+    var endpoint: URL? {
+        URL(string: "\(RequestConstants.baseURL)/api/v1/orders/1")
+    }
+
+    var httpMethod: HttpMethod { .put }
+
+    var dto: Dto? {
+        DtoModel(nftIds: nftIds)
+    }
+
+    var headers: [String: String] {
+        [
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Practicum-Mobile-Token": RequestConstants.token
+        ]
+    }
+}
