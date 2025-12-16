958131f2a92eeb8cdc41f6ccf55065a13eae5091
Damir Salakhetdinov <saldam@inbox.ru>
2025-12-16 14:17:43 +0300

    (fix) untracked files added

---
+++ FakeNFT/SceneDelegate.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    let servicesAssembly = ServicesAssembly(
+        networkClient: DefaultNetworkClient(),
+        nftStorage: NftStorageImpl()
+    )
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: windowScene)
+        
+        let splashViewController = SplashViewController()
+        
+        splashViewController.onFinish = { [weak self] in
+            
+            self?.showMainScreen()
+        }
+        
+        window?.rootViewController = splashViewController
+        window?.makeKeyAndVisible()
+    }
+    
+    private func showMainScreen() {
+        guard let window = window else { return }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let tabBarController = storyboard.instantiateInitialViewController() as? TabBarController else {
+            return
+        }
+        
+        tabBarController.servicesAssembly = servicesAssembly
+        
+        UIView.transition(
+            with: window,
+            duration: 0.3,
+            options: .transitionCrossDissolve,
+            animations: {
+                window.rootViewController = tabBarController
+            },
+            completion: nil
+        )
+    }
+}
